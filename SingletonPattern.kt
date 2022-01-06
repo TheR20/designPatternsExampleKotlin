35142e9ecf5a06dd611b178170072f60258289d4
TheR20 <41134429+TheR20@users.noreply.github.com>
2022-01-05 21:16:46 -0600

    Create SingletonPattern.kt

---
+++ SingletonPattern.kt
@@ -0,0 +1,15 @@
+objetc Manager{
+    init{
+          println("Inicio de un instancia")
+        }
+}
+
+fun main(){
+  pintln(Manager())
+  pintln(Manager())
+}
+
+//output result :
+
+//Manager@3454d4
+//Manager@3454d4
